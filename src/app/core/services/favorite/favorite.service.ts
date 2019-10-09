2231a7832e8835e7c9707ba44b4d178bff8abd9d
Walter V. Faustine <waltervfaustine@gmail.com>
2019-10-09 18:03:20 +0300

    refactor: 💡 modified favorite service by changing URL for chart
    This commit will modify favorite URL for call favorite data from API

--- src/app/core/services/favorite/favorite.service.ts
+++ src/app/core/services/favorite/favorite.service.ts
@@ -9,8 +9,7 @@ export class FavoriteService {
   constructor(private httpClient: NgxDhis2HttpClientService) {}
 
   getCascadeFavorite(): Observable<any> {
-    const favoriteURL =
-      'analytics?dimension=dx:xXrwhpYL4UD;itIVeB2QV3B;Zq7AbV0d2SS;BHUTFEEteC8&dimension=pe:LAST_QUARTER&filter=ou:m0frOspS7JY&displayProperty=NAME&skipMeta=true&includeNumDen=true';
+    const favoriteURL = `charts/XB4mc3920AY.json?fields=*`;
     return this.httpClient.get(favoriteURL);
   }
 }
