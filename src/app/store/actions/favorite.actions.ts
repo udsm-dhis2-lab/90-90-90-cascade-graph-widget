3d1da0b425f864b93a719055052a69b55ede8c2e
Walter V. Faustine <waltervfaustine@gmail.com>
2019-10-09 16:34:54 +0300

    refactor: 💡 modified store actions for the loading favorites
    This commit will modify the action definition for loading chart
    favorites

--- src/app/store/actions/favorite.actions.ts
+++ src/app/store/actions/favorite.actions.ts
@@ -1,41 +1,49 @@
-import { createAction, props, Action } from '@ngrx/store';
-import { ChartFavorite } from 'src/app/core/models/favorite.model';
+import { createAction, props } from '@ngrx/store';
+import { Favorite } from 'src/app/core/models/favorite.model';
+import { ErrorMessage } from 'src/app/core/models/error-message.model';
 
-export enum FavoriteActionTypes {
-    LoadDashboardFavorite = '[DASHBOARD FAVORITES] Load Dashboard Favorites',
-    LoadDashboardFavoriteSuccess = '[DASHBOARD FAVORITES] Load Dashboard Favorite Success',
-    LoadDashboardFavoriteFail = '[DASHBOARD FAVORITES] Load Dashboard Favorite Fail',
-    LoadFavoriteAnalytics = '[DASHBOARD FAVORITES ANALYTICS] Load Dashboard Favorites',
-    LoadFavoriteAnalyticsSuccess = '[DASHBOARD FAVORITES ANALYTICS] Load Dashboard Favorite Success',
-    LoadFavoriteAnalyticsFail = '[DASHBOARD FAVORITES ANALYTICS] Load Dashboard Favorite Fail',
+export enum FavoriteActionsDefinition {
+    LoadDashboardFavorite = '[FAVORITE] Load Dashboard Favorites',
+    LoadDashboardFavoriteSuccess = '[FAVORITE] Load Dashboard Favorite Success',
+    LoadDashboardFavoriteFail = '[FAVORITE] Load Dashboard Favorite Fail',
 }
 
-export class LoadDashboardFavorite implements Action {
-    readonly type = FavoriteActionTypes.LoadDashboardFavorite;
-    constructor(public payload: ChartFavorite) { }
-}
+export const LoadDashboardFavorite = createAction(
+    FavoriteActionsDefinition.LoadDashboardFavorite
+);
 
-export class LoadDashboardFavoriteSuccess implements Action {
-    readonly type = FavoriteActionTypes.LoadDashboardFavoriteSuccess;
-    constructor(public payload: ChartFavorite) { }
-}
+export const LoadDashboardFavoriteSuccess = createAction(
+    FavoriteActionsDefinition.LoadDashboardFavoriteSuccess,
+    props<{ chartFavorite: Favorite }>()
+);
 
-export class LoadDashboardFavoriteFail implements Action {
-    readonly type = FavoriteActionTypes.LoadFavoriteAnalytics;
-    constructor(public payload: string) { }
-}
+export const LoadDashboardFavoriteFail = createAction(
+    FavoriteActionsDefinition.LoadDashboardFavoriteFail,
+    props<{ error: ErrorMessage }>()
+);
 
-export class LoadFavoriteAnalytics implements Action {
-    readonly type = FavoriteActionTypes.LoadDashboardFavorite;
-    constructor(public payload: ChartFavorite) { }
-}
+// export enum FavoriteActionsDef {
+//     LoadDashboardFavorite = '[FAVORITE] Load Dashboard Favorites',
+//     LoadDashboardFavoriteSuccess = '[FAVORITE] Load Dashboard Favorite Success',
+//     LoadDashboardFavoriteFail = '[FAVORITE] Load Dashboard Favorite Fail',
+// }
 
-export class LoadFavoriteAnalyticsSuccess implements Action {
-    readonly type = FavoriteActionTypes.LoadFavoriteAnalyticsSuccess;
-    constructor(public payload: ChartFavorite) { }
-}
+// export class LoadDashboardFavorite implements Action {
+//     readonly type = FavoriteActionsDef.LoadDashboardFavorite;
+//     constructor(public payload: Favorite) { }
+// }
 
-export class LoadFavoriteAnalyticsFail implements Action {
-    readonly type = FavoriteActionTypes.LoadFavoriteAnalyticsFail;
-    constructor(public payload: string) { }
-}
+// export class LoadDashboardFavoriteSuccess implements Action {
+//     readonly type = FavoriteActionsDef.LoadDashboardFavoriteSuccess;
+//     constructor(public payload: Favorite) { }
+// }
+
+// export class LoadDashboardFavoriteFail implements Action {
+//     readonly type = FavoriteActionsDef.LoadDashboardFavoriteFail;
+//     constructor(public payload: string) { }
+// }
+
+// export type FavoritesActionTypes =
+//     | LoadDashboardFavorite
+//     | LoadDashboardFavoriteSuccess
+//     | LoadDashboardFavoriteFail;
--- src/app/store/actions/index.ts
+++ src/app/store/actions/index.ts
@@ -1,3 +1,7 @@
 export * from './user.actions';
 export * from './router.actions';
 export * from './system-info.actions';
+export * from './analytics.action';
+// export * from './extension.action';
+export * from './favorite.actions';
+
