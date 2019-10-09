a87fb28b82b66de8d77d79009db1ef24892f02e9
Walter V. Faustine <waltervfaustine@gmail.com>
2019-10-09 17:45:57 +0300

    refactor: 💡 modified effect for loading favorites
    This commit will modify the effects for loading favorites

--- src/app/store/effects/index.ts
+++ src/app/store/effects/index.ts
@@ -1,5 +1,11 @@
 import { UserEffects } from './user.effects';
 import { SystemInfoEffects } from './system-info.effects';
 import { RouterEffects } from './router.effects';
+import { FavoriteEffects } from './chart-favorite.effect';
 
-export const effects: any[] = [UserEffects, SystemInfoEffects, RouterEffects];
+export const effects: any[] = [
+    UserEffects,
+    SystemInfoEffects,
+    RouterEffects,
+    FavoriteEffects,
+];
