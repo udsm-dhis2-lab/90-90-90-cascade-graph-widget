a9536697f0cef04bee8f8c1273cd93583e4949d8
Walter V. Faustine <waltervfaustine@gmail.com>
2019-10-09 17:57:53 +0300

    refactor: 💡 modified selectors for selecting favorite details
    This commit will modify selectors for selecting favorite details from
    store

---
+++ src/app/store/selectors/chart-favorite.selector.ts
@@ -0,0 +1,46 @@
+import { createSelector } from '@ngrx/store';
+import { getRootState, State } from '../reducers';
+import { FavoriteState, adapter } from '../states/favorite.state';
+
+export const getFavoriteSnapshot = createSelector(
+    getRootState,
+    (state: State) => state.favorite
+);
+
+export const getFavorite = createSelector(
+    getFavoriteSnapshot,
+    (favoriteState: FavoriteState) => favoriteState.chartFavorite
+);
+
+export const {
+    selectEntities: getFavoriteEntities,
+    selectAll: getAllFavorites,
+    selectIds: getFavoriteIds,
+    selectTotal: getTotalFavorites,
+} = adapter.getSelectors(getFavoriteSnapshot);
+
+export const getListOfFavorites = createSelector(
+    getFavoriteSnapshot,
+    (favoriteState: FavoriteState) => favoriteState.chartFavorite
+);
+
+
+export const getFavoriteLoading = createSelector(
+    getFavoriteSnapshot,
+    (favoriteState: FavoriteState) => favoriteState.loading
+);
+
+export const getFavoriteLoaded = createSelector(
+    getFavoriteSnapshot,
+    (favoriteState: FavoriteState) => favoriteState.loaded
+);
+
+export const getFavoriteHasError = createSelector(
+    getFavoriteSnapshot,
+    (favoriteState: FavoriteState) => favoriteState.hasError
+);
+
+export const getFavoriteError = createSelector(
+    getFavoriteSnapshot,
+    (favoriteState: FavoriteState) => favoriteState.error
+);
--- src/app/store/selectors/index.ts
+++ src/app/store/selectors/index.ts
@@ -1,3 +1,5 @@
 export * from './router.selectors';
 export * from './system-info.selectors';
 export * from './user.selectors';
+export * from './chart-favorite.selector';
+
