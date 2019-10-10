da9e74e2780aa84fa3d431e3927997e860f36407
Walter V. Faustine <waltervfaustine@gmail.com>
2019-10-10 07:54:57 +0300

    refactor: 💡 added observable type Favorite for API result
    This commit will add observable type for data returned through API
    Result

--- src/app/core/services/favorite/favorite.service.ts
+++ src/app/core/services/favorite/favorite.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { NgxDhis2HttpClientService } from '@hisptz/ngx-dhis2-http-client';
 import { Observable } from 'rxjs';
+import { Favorite } from '../../models/favorite.model';
 
 @Injectable({
   providedIn: 'root',
@@ -8,7 +9,7 @@ import { Observable } from 'rxjs';
 export class FavoriteService {
   constructor(private httpClient: NgxDhis2HttpClientService) {}
 
-  getCascadeFavorite(): Observable<any> {
+  getCascadeFavorite(): Observable<Favorite> {
     const favoriteURL = `charts/XB4mc3920AY.json?fields=*`;
     return this.httpClient.get(favoriteURL);
   }
