b58cca37172ce5ae65e73d46d17acbc8901e37cc
Walter V. Faustine <waltervfaustine@gmail.com>
2019-10-10 11:08:38 +0300

    refactor: 💡 modified index.ts file for exporting store contents
    This commit will modify index.ts file for exporting actions, reducers,
    effects and states

--- src/app/store/reducers/index.ts
+++ src/app/store/reducers/index.ts
@@ -8,13 +8,19 @@ import { UserState } from '../states/user.state';
 import { systemInfoReducer } from './system-info.reducer';
 import { userReducer } from './user.reducer';
 import { FavoriteState } from '../states/favorite.state';
-import { favoriteReducer } from './chart-favorite.reducer';
+import { favoriteReducer } from './favorite.reducer';
+import { extensionReducer } from './extension.reducer';
+import { ExtensionState } from '../states/extension.state';
+import { AnalyticsState } from '../states/analytics.state';
+import { analyticsReducer } from './analytics.reducer';
 
 export interface State {
   user: UserState;
   systemInfo: SystemInfoState;
   router: RouterReducerState;
   favorite: FavoriteState;
+  extension: ExtensionState;
+  analytics: AnalyticsState;
 }
 
 export const reducers: ActionReducerMap<State> = {
@@ -22,6 +28,8 @@ export const reducers: ActionReducerMap<State> = {
   systemInfo: systemInfoReducer,
   router: routerReducer,
   favorite: favoriteReducer,
+  extension: extensionReducer,
+  analytics: analyticsReducer
 };
 
 export const metaReducers: MetaReducer<State>[] = !environment.production
