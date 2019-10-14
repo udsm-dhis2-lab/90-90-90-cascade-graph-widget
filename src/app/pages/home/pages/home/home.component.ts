e2f5e914c62678fcfe39843717b995ed8753ea0a
Walter V. Faustine <waltervfaustine@gmail.com>
2019-10-14 17:18:31 +0300

    feat: 🎸 added cascade visualization in the widget
    This commit will add support for cascade visualization in the dashboard
    widget

--- src/app/pages/home/pages/home/home.component.ts
+++ src/app/pages/home/pages/home/home.component.ts
@@ -4,6 +4,12 @@ import {
   getFavorites,
   getAnalytics,
   getExtension,
+  getFavoriteLoaded,
+  getAnalyticsLoaded,
+  getExtensionLoaded,
+  getFavoriteLoading,
+  getAnalyticsLoading,
+  getExtensionLoading,
 } from 'src/app/store/selectors';
 import { State } from 'src/app/store/reducers';
 import { LoadFavorite } from 'src/app/store/actions/favorite.actions';
@@ -17,7 +23,13 @@ import { Analytics } from 'src/app/core/models/analytics.model';
 import { Extension } from 'src/app/core/models/extension.model';
 import { drawChart } from 'src/app/core/helpers/draw-visualization.helper';
 import * as _ from 'lodash';
-import { getSanitizedAnalytics } from 'src/app/core/helpers/sanitized-analytics';
+import {
+  getSanitizedAnalytics,
+  sanitizeAnalyticsBasedOnConfiguration,
+} from 'src/app/core/helpers/sanitize-analytics';
+import { GenerateCascadeGraph } from '@iapps/visualization/cascade';
+import { extendOtherChartOptions } from 'src/app/core/helpers/draw-chart.helper';
+import { chart } from 'highcharts';
 
 @Component({
   selector: 'app-home',
@@ -25,9 +37,20 @@ import { getSanitizedAnalytics } from 'src/app/core/helpers/sanitized-analytics'
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  renderId: string;
+  chartHeight: string;
   favorite$: Observable<Favorite>;
   analytics$: Observable<Analytics>;
   extension$: Observable<Extension>;
+  favoriteLoaded$: Observable<boolean>;
+  analyticsLoaded$: Observable<boolean>;
+  extensionLoaded$: Observable<boolean>;
+  favoriteLoading$: Observable<boolean>;
+  analyticsLoading$: Observable<boolean>;
+  extensionLoading$: Observable<boolean>;
+  progressMSG: string;
+
+  cascadeChartOptions: any;
 
   constructor(private store: Store<State>) { }
 
@@ -36,9 +59,20 @@ export class HomeComponent implements OnInit {
     this.favorite$ = this.store.select(getFavorites);
     this.analytics$ = this.store.select(getAnalytics);
     this.extension$ = this.store.select(getExtension);
+    this.favoriteLoaded$ = this.store.select(getFavoriteLoaded);
+    this.analyticsLoaded$ = this.store.select(getAnalyticsLoaded);
+    this.extensionLoaded$ = this.store.select(getExtensionLoaded);
+    this.favoriteLoading$ = this.store.select(getFavoriteLoading);
+    this.analyticsLoading$ = this.store.select(getAnalyticsLoading);
+    this.extensionLoading$ = this.store.select(getExtensionLoading);
+    this.chartHeight = '520px';
+    this.progressMSG =
+      'Cascade Analysis to Quantify Progress and Gaps towards the 90-90-90 Targets';
 
     this.favorite$.subscribe((favorite: Favorite) => {
       if (favorite) {
+        this.renderId = favorite.id;
+        
         const visualizationLayout: VisualizationLayout = getVisualizationLayout();
         const visualizationConfiguration: ChartConfiguration = getChartConfiguration(
           favorite,
@@ -46,24 +80,59 @@ export class HomeComponent implements OnInit {
           visualizationLayout
         );
 
-        this.analytics$.subscribe((analytic: Analytics) => {
-          if (analytic) {
+        this.analytics$.subscribe((analytics: Analytics) => {
+          if (analytics) {
             this.extension$.subscribe((extensions: any) => {
               if (extensions) {
                 const cascadeExtension: Extension = _.filter(
                   extensions,
                   (extension: Extension) => extension.id === favorite.id
                 )[0];
-                const final = drawChart(
-                  analytic,
+
+                const sanitizedAnalytics = getSanitizedAnalytics(
+                  favorite,
+                  analytics
+                );
+
+                const chartAnalytics = sanitizeAnalyticsBasedOnConfiguration(
+                  sanitizedAnalytics,
+                  visualizationConfiguration
+                );
+
+                const initialChartObject = drawChart(
+                  chartAnalytics,
                   visualizationConfiguration,
                   cascadeExtension
                 );
+
+                const chartObject = extendOtherChartOptions(
+                  initialChartObject,
+                  chartAnalytics,
+                  visualizationConfiguration
+                );
+                this.cascadeChartOptions = this.drawCascadeChart(
+                  cascadeExtension,
+                  chartObject
+                );
+                chart(this.renderId, this.cascadeChartOptions);
               }
             });
           }
         });
       }
     });
+  }
+
+  drawCascadeChart(chartExtension: Extension, chartObject: any) {
+    return GenerateCascadeGraph(
+      true,
+      true,
+      'nacp',
+      'dhis2',
+      'column',
+      chartObject,
+      chartExtension,
+      1600000
+    );
   }
 }
--- src/app/pages/home/pages/index.ts
+++ src/app/pages/home/pages/index.ts
@@ -1,3 +1,7 @@
 import { HomeComponent } from './home/home.component';
+import { VisualizationCardLoaderComponent } from '../component/visualization-card-loader/visualization-card-loader.component';
 
-export const pages: any[] = [HomeComponent];
+export const pages: any[] = [
+    HomeComponent,
+    VisualizationCardLoaderComponent,
+];
